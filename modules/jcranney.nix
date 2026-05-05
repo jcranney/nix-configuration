9ddc76bc8f22eff0fad96b72e1736d6ae8f8b7f9
Jesse Cranney <jesse.cranney@anu.edu.au>
2026-05-05 13:29:45 +1000

    this also seems to be working now.

--- modules/jcranney.nix
+++ modules/jcranney.nix
@@ -1,15 +1,6 @@
 {
   den.aspects.jcranney = {
     homeManager = { pkgs, ... }: {
-      # nixpkgs.config.packageOverrides = pkgs: {
-      #   nur = import (builtins.fetchTarball {
-      #     url = "https://github.com/nix-community/NUR/archive/main.tar.gz";
-      #     sha256 = "sha256:1p9jz85zij4crfj010zdl3p22qxhriv48l5p573jcdx6g10q8hnf";
-      #   }
-      #   ) {
-      #     inherit pkgs;
-      #   };
-      # };    
       home.packages = with pkgs; [ 
         hello
         vim vscode ripgrep htop tldr wget dig xclip # core cli/dev tools
@@ -23,25 +14,6 @@
         qbittorrent
         uv cargo rustc maturin clang openssl pkg-config  # python + rust (until I master flakes)
         xournalpp libreoffice vlc imagemagickBig  # normal human stuff
-        (rustPlatform.buildRustPackage rec {
-          pname = "para-audit";
-          version = "v0.1.15";
-          cargoHash = "sha256-AAeykJIyNNw6xm4g0YBDb3zuNYj/8AKhDAHu9PtUNLs=";
-          nativeBuildInputs = [ pkg-config ];
-          buildInputs = [ openssl ];
-          src = fetchgit {
-            url = "https://github.com/jcranney/para-audit.git";
-            tag = version;
-            hash = "sha256-bS0Up3MBUHEZclTOHLo6QWhmgJH7cam6/XJnnzD4UpY=";
-          };
-          meta = with lib; {
-            description = "A tool for auditing/organising/interacting with my para system.";
-            homepage = https://github.com/jcranney/para-audit;
-            license = licenses.unlicense;
-            platforms = platforms.all;
-            mainProgram = "para";
-          };
-        })
       ];
       programs.git = {
         enable = true;
---
+++ modules/para.nix
@@ -0,0 +1,28 @@
+{
+  den.aspects.jcranney = {
+    homeManager = { pkgs, ... }: {
+      home.packages = with pkgs; [ 
+        cowsay
+        (rustPlatform.buildRustPackage rec {
+          pname = "para-audit";
+          version = "v0.1.15";
+          cargoHash = "sha256-AAeykJIyNNw6xm4g0YBDb3zuNYj/8AKhDAHu9PtUNLs=";
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [ openssl ];
+          src = fetchgit {
+            url = "https://github.com/jcranney/para-audit.git";
+            tag = version;
+            hash = "sha256-bS0Up3MBUHEZclTOHLo6QWhmgJH7cam6/XJnnzD4UpY=";
+          };
+          meta = with lib; {
+            description = "A tool for auditing/organising/interacting with my para system.";
+            homepage = https://github.com/jcranney/para-audit;
+            license = licenses.unlicense;
+            platforms = platforms.all;
+            mainProgram = "para";
+          };
+        })
+      ];
+    };
+  };
+}
