c60ec835f33ff66b6c55052d76450e37daee2ebb
Jesse Cranney <jesse.cranney@anu.edu.au>
2026-05-05 13:21:57 +1000

    this works with para included

--- modules/jcranney.nix
+++ modules/jcranney.nix
@@ -16,13 +16,32 @@
         arduino-ide ffmpeg_7-full
         unzip tree kdePackages.yakuake gnumake watchexec
         insync # nur.repos.jcranney.para-audit  # filesystem/organisation
-        # openscad freecad
-        # prusa-slicer inkscape  # design/3d printing
+        openscad freecad
+        prusa-slicer inkscape  # design/3d printing
         slack yed zoom-us quickemu graphviz subversion gpclient  # aitc projects
         texliveFull yed mermaid-cli  # ultiamte subaru
         qbittorrent
         uv cargo rustc maturin clang openssl pkg-config  # python + rust (until I master flakes)
         xournalpp libreoffice vlc imagemagickBig  # normal human stuff
+        (rustPlatform.buildRustPackage rec {
+          pname = "para-audit";
+          version = "v0.1.15";
+          cargoHash = "sha256-AAeykJIyNNw6xm4g0YBDb3zuNYj/8AKhDAHu9PtUNLs=";
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [ openssl ];
+          src = fetchgit {
+            url = "https://github.com/jcranney/para-audit.git";
+            tag = version;
+            hash = "sha256-bS0Up3MBUHEZclTOHLo6QWhmgJH7cam6/XJnnzD4UpY=";
+          };
+          meta = with lib; {
+            description = "A tool for auditing/organising/interacting with my para system.";
+            homepage = https://github.com/jcranney/para-audit;
+            license = licenses.unlicense;
+            platforms = platforms.all;
+            mainProgram = "para";
+          };
+        })
       ];
       programs.git = {
         enable = true;
