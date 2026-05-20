cbff1002b23bc82d6fc1d603077d835ff22a72ee
Jesse Cranney <jesse.cranney@anu.edu.au>
2026-05-20 18:54:56 +1000

    updated, and bumped para to v0.1.16

--- modules/para.nix
+++ modules/para.nix
@@ -5,14 +5,14 @@
         cowsay
         (rustPlatform.buildRustPackage rec {
           pname = "para-audit";
-          version = "v0.1.15";
-          cargoHash = "sha256-AAeykJIyNNw6xm4g0YBDb3zuNYj/8AKhDAHu9PtUNLs=";
+          version = "v0.1.16";
+          cargoHash = "sha256-WI7xjYGaPgvnhNmzZc77v+rAxjxSwXpIhvzNFIVZqSY=";
           nativeBuildInputs = [ pkg-config ];
           buildInputs = [ openssl ];
           src = fetchgit {
             url = "https://github.com/jcranney/para-audit.git";
             tag = version;
-            hash = "sha256-bS0Up3MBUHEZclTOHLo6QWhmgJH7cam6/XJnnzD4UpY=";
+            hash = "sha256-/27GRBca2F2LnD8Pwh8EcoE0heeRMsVZ8cU13uzgnec=";
           };
           meta = with lib; {
             description = "A tool for auditing/organising/interacting with my para system.";
